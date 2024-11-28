d3308065c3d7f6976721cd3c2d9ced33450ca88b
RishabhRV4444 <rishabh@arevei.com>
2024-11-28 13:07:03 +0530

    update description

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 export const metadata = {
   title: "Jenii - A JP Sterling Silver Brand | Premium Sterling Silver Jewellery",
-  description: "Jenii Jewelry Shop offers a curated selection of exquisite silver jewelry, blending timeless elegance with modern designs. Our collection features meticulously crafted pieces, including rings, necklaces, bracelets, and earrings, all made from high-quality silver to ensure both style and durability. ",
+  description: "Jenii offers a premium selection of sterling silver jewellery for gift and wedding purpose, blending timeless elegance with modern designs. ",
   keywords: ["silver jewellery", "Jenii JP", "JP jewellers"," Silver Rings", "Silver Bracelets", "couple Rings", "jwellers for men"," jwellers for women" ,"silver branded jewelleries"]
 };
 
